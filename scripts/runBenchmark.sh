f001eb8fa13b0a0192d9ac87a952b1513bd720f8
Steve Fox <steve@autoscalr.com>
2017-10-29 12:54:49 -0500

    Initial commit

--- scripts/runBenchmark.sh
+++ scripts/runBenchmark.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-# load the AutoScalr Terraform plugin
-git clone https://github.com/autoscalr/terraform-provider-autoscalr.git
+export PATH=$PATH:/usr/local/go/bin
+# load and build the AutoScalr Terraform plugin
+go get github.com/autoscalr/terraform-provider-autoscalr
+cd ~/go/src/github.com/autoscalr/terraform-provider-autoscalr/
+./build.sh
+cd
+mkdir -p ~/terraform/terraform.d/plugins/linux_amd64
+cp ~/go/src/github.com/autoscalr/terraform-provider-autoscalr/terraform-provider-autoscalr ~/terraform/terraform.d/plugins/linux_amd64
+# load aws-cost-analysis tools
 git clone https://github.com/concurrencylabs/aws-cost-analysis.git
 sudo pip install -r aws-cost-analysis/requirements.txt
